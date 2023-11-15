979775e5aca96f7b2de01a862ed01b5f511532ca
jamazoz <69641292+jamazoz@users.noreply.github.com>
2023-11-15 16:56:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,11 +53,7 @@ def text_to_speech(text, tld):
 #display_output_text = st.checkbox("Verifica el texto")
 
 if st.button("Encender luz"):
-    result, output_text = text_to_speech(text, tld)
-    audio_file = open(f"temp/{result}.mp3", "rb")
-    audio_bytes = audio_file.read()
     st.markdown(f"## Tú audio:")
-    st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
     #if display_output_text:
     st.markdown(f"## Texto en audio:")
@@ -65,8 +61,8 @@ if st.button("Encender luz"):
 
 
 if st.button("Apagar luz"):
-
-
+    st.markdown(f"## Texto en audio:")
+    st.write(f" {output_text}"
 
 
 
